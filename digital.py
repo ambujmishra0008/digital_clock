50f85695af5fa8f48238172c7b64e875a4e5c866
ambujmishra0008 <ambujmishra0008@gmail.com>
2023-03-30 15:57:06 +0530

    digital clock is added in ambuj clock

--- digital.py
+++ digital.py
@@ -1,9 +1,16 @@
 from tkinter import Tk, Label
-
+from datetime import datetime 
 window = Tk()
-window.title("Digital Clock")
+window.title("Digital Ambuj Clock")
 window.geometry("600x300")
 window.configure(bg="red")
-label = Label(window,text="Adarsh Clock", font=("Arial Black", 78, "bold"), bg="steelblue")
+label = Label(window,text="Ambuj Clock", font=("Arial Black", 78, "bold"), bg="steelblue")
 label.pack(pady=100)
+
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500, clock)
+clock()
 window.mainloop()
